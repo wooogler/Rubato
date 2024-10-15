9438e8104f7c0f29ad2e864b83550e927b1e13b7
Sangwook Lee <leesang627@gmail.com>
2024-10-15 18:45:01 -0400

    change URL

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import io from "socket.io-client";
 import "./App.css";
 
-const socket = io("http://localhost:5000", {
+const socket = io("http://100.64.233.45:5000", {
   transports: ["websocket"],
   extraHeaders: {
     "Access-Control-Allow-Origin": "http://localhost:5173",
